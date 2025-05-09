e016e0fbe1255af865743785ef8e648e440456dd
Fetsum914 <fitsumabyu914@gmail.com>
2025-05-09 18:31:25 +0300

    initial

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse, Say
+
+app = Flask(__name__)
+
+
+@app.route('/sms', methods=['POST'])
+def sms():
+    print(request.form)
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    resp = MessagingResponse()
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    
+    return str(resp)
+
+
+@app.route("/voice", methods=['POST'])
+def voice():
+    response = VoiceResponse()
+    say = Say('Hi', voice='Polly.Emma')
+    say.break_(strength='x-weak', time='100ms')
+    say.p('Welcome Nexus Labs.')
+    say.p('Please, leave me a message.')
+    say.break_(strength='x-weak', time='50ms')
+    say.p('Goodbye!')
+
+    response.append(say)
+    response.record()
+    response.hangup()
+
+    return str(response)
+
+
+@app.route('/')
+def index():
+    return """<p>Nexus Labs</p>
+    """
+
+
+if __name__ == '__main__':
+    app.run(host='phone.tonyteaches.tech', port=5000)
+
